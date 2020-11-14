54462880ad6c6d5705d4e336c495e1ea5913aa08
alex li <alelia40@gmail.com>
2020-11-14 13:21:06 -0500

    start of functionality, theoretically finds available moves for a board state

--- ai.js
+++ ai.js
@@ -1,8 +1,44 @@
+function checkWinner() {
+ //how do?
+}
+
+function getAvailableMoves(board) {
+  let availableMoves = [];
+  let move;
+  for( let i = 0; i < 7 ; i++) {
+    move = columnMove(i);
+    if(move != [-1, -1]) {
+      availableMoves.push(move);
+    }
+  }
+
+  return availableMoves;
+}
+
+function columnMove(column) {
+
+  for( let i = 0; i < 6; i++) {
+    if(board[i][column] === 0) {
+      return [i, column];
+    }
+  }
+
+  return [-1, -1];
+}
+
 function getMove(player, board) {
   // TODO: Determine valid moves
   // TODO: Calculate best move
   console.log(player);
   console.log(board);
+
+  console.log(getAvailableMoves(board));
+
+  //find available moves
+
+  //loop through and find scores using algorithm
+
+  //pick best one
 
   return {column: 1};
 }
