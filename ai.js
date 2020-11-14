b8b336bb71b4447172fbd0130c48e0323bb3c62d
alex li <alelia40@gmail.com>
2020-11-14 15:51:04 -0500

    fixes bug with available moves, starts win states and adds tome test stuff

--- ai.js
+++ ai.js
@@ -3,17 +3,59 @@ function checkWinner(board) {
  //how do?
  let winner = "";
 
+ //tie occurs if there are no moves and no winner yet
  if(getAvailableMoves(board).length == 0 && winner == "") {
   return "Tie";
  }
 }
 
+/**
+ * helper function that checks for a win horizontally
+ * @param {*} board 
+ */
+function checkForHorizontalWin(board) {
+  let p1count = 0;
+  let p2count = 0;
+  for( let i = 5; i >= 0; i--) {
+    for(let j = 0; j < 7; j++) {
+      //increment counters for each element checked on the row
+      if(board[i][j] == 1) {
+        p2count = 0;
+        p1count++;
+      } else if (board[i][j] == 2) {
+        p1count = 0;
+        p2count++;
+      } else {
+        p2count = 0;
+        p1count = 0;
+      }
+
+      //if there is 4 then return a win
+      if(p1count == 4) {
+        return "1";
+      } else if(p2count === 4) {
+        return "2";
+      }
+    }
+
+    //reset counts before changing rows
+    p1count = 0;
+    p2count = 0;
+  }
+
+  return null; //if no win found
+}
+
+/**
+ * function that finds all available next moves for a given board state
+ * @param {*} board 
+ */
 function getAvailableMoves(board) {
   let availableMoves = [];
   let move;
   for( let i = 0; i < 7 ; i++) {
     move = columnMove(board, i);
-    if(move != [-1, -1]) {
+    if(move !== null) {
       availableMoves.push(move);
     }
   }
@@ -21,6 +63,11 @@ function getAvailableMoves(board) {
   return availableMoves;
 }
 
+/**
+ * helper function that returns the first available move on one column
+ * @param {*} board 
+ * @param {*} column 
+ */
 function columnMove(board, column) {
 
   for( let i = 5; i >= 0; i--) {
@@ -29,7 +76,7 @@ function columnMove(board, column) {
     }
   }
 
-  return [-1, -1];
+  return null;
 }
 
 function getMove(player, board) {
@@ -40,9 +87,9 @@ function getMove(player, board) {
 
   //find available moves
   let availableMoves = getAvailableMoves(board);
+  console.log(availableMoves);
 
   let randomColumn = availableMoves[Math.floor(Math.random() * availableMoves.length)][1];
-
   //loop through and find scores using algorithm
 
   //pick best one
@@ -56,4 +103,4 @@ function prepareResponse(move) {
   return response;
 }
 
-module.exports = { getMove, prepareResponse };
+module.exports = { getMove, prepareResponse, getAvailableMoves, checkForHorizontalWin };
--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const ai = require('./ai');
 test('returns a valid response', () => {
   expect(ai.prepareResponse({column: 1})).toEqual(`{"column":1}\n`)
 });
+
+test('edge case for next available moves', () => {
+  expect(ai.getAvailableMoves([[1,1,1,1,1,1,1],[1,1,1,1,1,1,1],[1,1,1,1,1,1,1],[1,1,1,1,1,1,1],[1,1,1,1,1,1,1],[1,1,1,1,1,1,1]])).toEqual([]);
+})
