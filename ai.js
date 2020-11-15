2ecae56d79de8a626843168ba619998a77404312
alex li <alelia40@gmail.com>
2020-11-15 14:28:25 -0500

    adds in move blocking functionality

--- ai.js
+++ ai.js
@@ -139,6 +139,70 @@ function checkForDiagonalWin(board) {
   return winner;
 }
 
+/**
+ * searches 3 spaces from each value in the table in diagonal directions for possible connect 4, returns location to block
+ * @param {*} board 
+ */
+function checkForBlock(board, currentPlayer) {
+
+  let opposingPlayer = 1; //get the opposing player number
+  if (currentPlayer == 1) {
+    opposingPlayer = 2;
+  }
+
+  let block = null;
+
+  let directions = [[0, -1], [1, 0], [1, -1], [-1, -1]]; //2 diagonal directions, top left to bottom right + top right to bottom left, horizontal left to right and vertical top to bottom
+  let currentDirection;
+  for (let d = 0; d < 4; d++) {
+    //set slant
+    currentDirection = directions[d];
+    let slantx = currentDirection[0];
+    let slanty = currentDirection[1];
+
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 7; j++) {
+        //set an end coordinate 3 away in a diagonal direction
+        let xend = j + (3 * slantx);
+        let yend = i + (3 * slanty);
+
+        if (0 <= xend && xend < 7 && 0 <= yend && yend < 6) { //only evaluate the node if we can get a diagonal from that position (not out of bounds)
+          let space1 = board[i][j];
+          let space2 = board[i + slanty][j + slantx];
+          let space3 = board[i + (2 * slanty)][j + (2 * slantx)];
+          let space4 = board[yend][xend];
+          if (space1 == opposingPlayer && space2 == opposingPlayer && space3 == opposingPlayer && space4 == 0) { //if there's a potential win for the opponent then there is danger
+            block = [yend, xend]; //column to block
+          } else if (space1 == 0 && space2 == opposingPlayer && space3 == opposingPlayer && space4 == opposingPlayer) {
+            block = [i, j]; //column to block
+          } else if (space1 == opposingPlayer && space2 == 0 && space3 == opposingPlayer && space4 == opposingPlayer) {
+            block = [i + slanty, j + slantx]; //column to block
+          } else if (space1 == opposingPlayer && space2 == opposingPlayer && space3 == 0 && space4 == opposingPlayer) {
+            block = [i + (2 * slanty), j + (2 * slantx)]; //column to block
+          }
+        }
+      }
+    }
+  }
+
+  if (block != null) {
+    let availableMoves = getAvailableMoves(board);
+    let blockIsAvailable = false;
+    for (let i = 0; i < availableMoves.length; i++) {
+      if (availableMoves[i][0] == block[0] && availableMoves[i][1] == block[1]) {
+        blockIsAvailable = true;
+      }
+    }
+
+    if (!blockIsAvailable) {
+      block = null;
+    }
+  }
+
+
+  return block;
+}
+
 
 /**
  * function that finds all available next moves for a given board state
@@ -195,7 +259,7 @@ var STRATEGY = "defend"; //constant that tracks the strategy that AI is trying
  * helper function that switches the strategey constant
  */
 function switchStrategy() {
-  if(STRATEGY === "defend") {
+  if (STRATEGY === "defend") {
     STRATEGY = "attack";
   } else {
     STRATEGY = "defend";
@@ -209,63 +273,74 @@ function switchStrategy() {
  */
 function getMove(player, board) {
 
-  //find available moves
-  let availableMoves = getAvailableMoves(board);
-  let bestMove;
-  let bestScore;
-  if(STRATEGY == "defend") {
-    bestScore = Number.NEGATIVE_INFINITY;
-  } else {
-    bestScore = Number.POSITIVE_INFINITY;
-  }
-    
- 
-  MOVES_LEFT--;
-  console.log("depth:"+MOVES_LEFT);
+  let block = checkForBlock(board, player); //check for an available block
+  if (block != null) {
+    return { column: block[1] }; //make the blocking move
+  } else { //otherwise find the best move for us
 
-  for (let i = 0; i < availableMoves.length; i++) {
-    //evaluate each move with algorithm
-    let move = availableMoves[i];
+    //find available moves
+    let availableMoves = getAvailableMoves(board);
 
-    board[move[0]][move[1]] = player;
-
-    let score;
-    if(STRATEGY == "defend") {
-      score = minimaxab(player, board, MOVES_LEFT, true, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+    let bestMove;
+    let bestScore;
+    if (STRATEGY == "defend") {
+      bestScore = Number.NEGATIVE_INFINITY;
     } else {
-      score = minimaxab(player, board, MOVES_LEFT, false, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+      bestScore = Number.POSITIVE_INFINITY;
     }
 
-    board[move[0]][move[1]] = 0;
+    MOVES_LEFT--;
+    console.log("depth:" + MOVES_LEFT);
 
-    console.log(move + " - " + score);
+    for (let i = 0; i < availableMoves.length; i++) {
+      //evaluate each move with algorithm
+      let move = availableMoves[i];
 
-    if(STRATEGY === "defend") {
-      if (score > bestScore) {
-        bestScore = score;
-        bestMove = move;
-      } else if(score == bestScore) { //favor the middle column if the algorithm determines they have the same value
-        if( move[1] === 3) {
-          bestScore = score;
-          bestMove = move;
-        }
+      board[move[0]][move[1]] = player;
+
+      let score;
+      if (STRATEGY == "defend") {
+        score = minimaxab(player, board, MOVES_LEFT, true, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+      } else {
+        score = minimaxab(player, board, MOVES_LEFT, false, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
       }
-    } else {
-      if (score < bestScore) {
-        bestScore = score;
-        bestMove = move;
-      } else if(score == bestScore) { //favor the middle column if the algorithm determines they have the same value
-        if( move[1] === 3) {
+
+      //check for imminent danger if doing this move
+      if (checkForBlock(board, player) != null) {
+        score = Number.NEGATIVE_INFINITY; //if this move will kill us then try to disqualify it
+      }
+
+      board[move[0]][move[1]] = 0;
+
+      console.log(move + " - " + score);
+
+      if (STRATEGY === "defend") {
+        if (score > bestScore) {
           bestScore = score;
           bestMove = move;
+        } else if (score == bestScore) { //favor the middle column if the algorithm determines they have the same value
+          if (move[1] === 3) {
+            bestScore = score;
+            bestMove = move;
+          }
+        }
+      } else {
+        if (score < bestScore) {
+          bestScore = score;
+          bestMove = move;
+        } else if (score == bestScore) { //favor the middle column if the algorithm determines they have the same value
+          if (move[1] === 3) {
+            bestScore = score;
+            bestMove = move;
+          }
         }
       }
     }
+
+    //switchStrategy(); //with each move the strategy will alternat, when attacking the AI will try to take the opponent's best move first
+
+    return { column: bestMove[1] };
   }
-
-  switchStrategy(); //with each move the strategy will alternat, when attacking the AI will try to take the opponent's best move first
-
-  return { column: bestMove[1] };
 }
 
 
@@ -277,7 +352,7 @@ function getMove(player, board) {
  */
 function minimaxab(playerNo, board, depth, maxplayer, alpha, beta) {
   let winner = checkWinner(board);
-  if(depth === 0) {
+  if (depth === 0) {
     return 0;
   }
   if (winner !== null) {
@@ -293,12 +368,12 @@ function minimaxab(playerNo, board, depth, maxplayer, alpha, beta) {
     for (let i = 0; i < availableMoves.length; i++) {
       move = availableMoves[i];
 
-      if(playerNo == 1) {
+      if (playerNo == 1) {
         board[move[0]][move[1]] = 1;
       } else {
         board[move[0]][move[1]] = 2;
       }
-      
+
       let score = minimaxab(playerNo, board, depth - 1, false, alpha, beta); //get score for move
       board[move[0]][move[1]] = 0;
 
@@ -309,7 +384,7 @@ function minimaxab(playerNo, board, depth, maxplayer, alpha, beta) {
         break;
       }
     }
-    
+
     return bestScore;
 
   } else { //minimizingplayer - player2
@@ -320,8 +395,8 @@ function minimaxab(playerNo, board, depth, maxplayer, alpha, beta) {
     let move;
     for (let i = 0; i < availableMoves.length; i++) {
       move = availableMoves[i];
-      
-      if(playerNo == 1) {
+
+      if (playerNo == 1) {
         board[move[0]][move[1]] = 1;
       } else {
         board[move[0]][move[1]] = 2;
@@ -337,7 +412,7 @@ function minimaxab(playerNo, board, depth, maxplayer, alpha, beta) {
         break;
       }
     }
-    
+
     return bestScore;
   }
 }
@@ -349,13 +424,13 @@ function minimaxab(playerNo, board, depth, maxplayer, alpha, beta) {
  */
 function heuristicVal(winner, depth, playerNo) {
   if (winner == "1") {
-    if (playerNo == 1){
+    if (playerNo == 1) {
       return depth;
     } else {
       return depth * -7;
     }
   } else if (winner == "2") {
-    if (playerNo == 2){
+    if (playerNo == 2) {
       return depth;
     } else {
       return depth * -7;
@@ -371,4 +446,4 @@ function prepareResponse(move) {
   return response;
 }
 
-module.exports = { getMove, prepareResponse, getAvailableMoves, checkWinner };
+module.exports = { getMove, prepareResponse, getAvailableMoves, checkWinner, checkForBlock };
